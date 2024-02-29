865455f9d9e8fb89cd57a79016023fbb63b501b2
ggarsevanishvili <154730875+ggarsevanishvili@users.noreply.github.com>
2024-02-29 20:04:14 +0400

    Update jeyran.js

--- assets/scripts/jeyran.js
+++ assets/scripts/jeyran.js
@@ -1,46 +1,57 @@
-let firstUserChoice = prompt(
-  "ჩაწერეთ თქვენი არჩევანი: ჭა, ქაღალდი, ან მაკრატელი:"
-);
+let circle = Infinity;
 
-while (
-  firstUserChoice !== "ჭა" &&
-  firstUserChoice !== "ქაღალდი" &&
-  firstUserChoice !== "მაკრატელი"
-) {
-  tryAgain = confirm(
-    "დაფიქსირდა შეცდომა! შეყვანილი მნიშვნელობა უნდა იყოს მხოლოდ: ჭა, ქაღალდი, ან მაკრატელი:"
-  );
-  firstUserChoice = prompt(
+while (circle === Infinity) {
+  let firstUserChoice = prompt(
     "ჩაწერეთ თქვენი არჩევანი: ჭა, ქაღალდი, ან მაკრატელი:"
   );
-}
 
-let secondUserChoice = prompt(
-  "ჩაწერეთ თქვენი მოწინააღმდეგის არჩევანი: ჭა, ქაღალდი, ან მაკრატელი:"
-);
+  while (
+    firstUserChoice !== "ჭა" &&
+    firstUserChoice !== "ქაღალდი" &&
+    firstUserChoice !== "მაკრატელი"
+  ) {
+    tryAgain = confirm(
+      "დაფიქსირდა შეცდომა! შეყვანილი მნიშვნელობა უნდა იყოს მხოლოდ: ჭა, ქაღალდი, ან მაკრატელი:"
+    );
+    firstUserChoice = prompt(
+      "ჩაწერეთ თქვენი არჩევანი: ჭა, ქაღალდი, ან მაკრატელი:"
+    );
+  }
 
-while (
-  secondUserChoice !== "ჭა" &&
-  secondUserChoice !== "ქაღალდი" &&
-  secondUserChoice !== "მაკრატელი"
-) {
-  tryAgain = confirm(
-    "დაფიქსირდა შეცდომა! შეყვანილი მნიშვნელობა უნდა იყოს მხოლოდ: ჭა, ქაღალდი, ან მაკრატელი:"
-  );
-
-  secondUserChoice = prompt(
+  let secondUserChoice = prompt(
     "ჩაწერეთ თქვენი მოწინააღმდეგის არჩევანი: ჭა, ქაღალდი, ან მაკრატელი:"
   );
-}
 
-if (firstUserChoice === secondUserChoice) {
-  result = alert("დაფიქსირდა ფრე");
-} else if (
-  (firstUserChoice === "ჭა" && secondUserChoice === "მაკრატელი") ||
-  (firstUserChoice === "ქაღალდი" && secondUserChoice === "ჭა") ||
-  (firstUserChoice === "მაკრატელი" && secondUserChoice === "ქაღალდი")
-) {
-  result = alert("გილოცავთ! თქვენ გაიმარჯვეთ.");
-} else {
-  result = alert("სამწუხაროდ თქვენ დამარცხდით");
+  while (
+    secondUserChoice !== "ჭა" &&
+    secondUserChoice !== "ქაღალდი" &&
+    secondUserChoice !== "მაკრატელი"
+  ) {
+    tryAgain = confirm(
+      "დაფიქსირდა შეცდომა! შეყვანილი მნიშვნელობა უნდა იყოს მხოლოდ: ჭა, ქაღალდი, ან მაკრატელი:"
+    );
+
+    secondUserChoice = prompt(
+      "ჩაწერეთ თქვენი მოწინააღმდეგის არჩევანი: ჭა, ქაღალდი, ან მაკრატელი:"
+    );
+  }
+
+  if (firstUserChoice === secondUserChoice) {
+    result = alert("დაფიქსირდა ფრე");
+  } else if (
+    (firstUserChoice === "ჭა" && secondUserChoice === "მაკრატელი") ||
+    (firstUserChoice === "ქაღალდი" && secondUserChoice === "ჭა") ||
+    (firstUserChoice === "მაკრატელი" && secondUserChoice === "ქაღალდი")
+  ) {
+    result = alert("გილოცავთ! თქვენ გაიმარჯვეთ.");
+  } else {
+    result = alert("სამწუხაროდ თქვენ დამარცხდით");
+  }
+  loop = confirm("გსურთ ხელახლა ცდა?");
+  {
+    if (!loop) {
+      alert("მადლობა, ნახვამდის");
+      break;
+    }
+  }
 }
